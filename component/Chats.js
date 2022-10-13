f74bc982319f800f3d5251c16c74f71e16ca82af
Austin <austinpaul606@gmail.com>
2022-10-13 20:35:36 +0100

    header section

---
+++ component/Chats.js
@@ -0,0 +1,14 @@
+import styles from "./Chats.module.css";
+const Chats = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.headers}>
+        <h2>Chats</h2>
+        <h2>Rooms</h2>
+        <h2>Friends</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
---
+++ component/Status.js
@@ -0,0 +1,44 @@
+import styles from "../styles/Home.module.css";
+const StatusUpdates = () => {
+  return (
+    <div className={styles.status__updates}>
+      <div className={styles.update__headers}>
+        <img
+          src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+          alt=""
+        />
+        <h2>Theo</h2>
+      </div>
+      <div className={styles.update__headers}>
+        <img
+          src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+          alt=""
+        />
+        <h2>Ann</h2>
+      </div>
+      <div className={styles.update__headers}>
+        <img
+          src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+          alt=""
+        />
+        <h2>Skelly</h2>
+      </div>
+      <div className={styles.update__headers}>
+        <img
+          src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+          alt=""
+        />
+        <h2>Jan49</h2>
+      </div>
+      <div className={styles.update__headers}>
+        <img
+          src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+          alt=""
+        />
+        <h2>Ann</h2>
+      </div>
+    </div>
+  );
+};
+
+export default StatusUpdates;
