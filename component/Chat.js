637d15ad085545f36089b3384dd3f0ff5969438c
Austin <austinpaul606@gmail.com>
2022-10-15 20:09:10 +0100

    chat headers

--- component/Chat.js
+++ component/Chat.js
@@ -20,6 +20,168 @@ const Chat = () => {
           <p>3</p>
         </div>
       </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>Poco84</p>
+            <p>Yooo!!</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p> Ron44</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>MArk</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>MArk</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>MArk</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>MArk</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>MArk</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>MArk</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
+      <div className={styles.chatsss}>
+        <div className={styles.chat__headers}>
+          <div className={styles.image}>
+            <img
+              src="https://www.irishexaminer.com/cms_media/module_img/275/137630_1_seoimage16x9_ie-401163_4bad92fd00a3493386706e00704e3488.jpg"
+              alt=""
+            />
+          </div>
+          <div className={styles.chatHeaders__details}>
+            <p>MArk</p>
+            <p>what you doing?</p>
+          </div>
+        </div>
+        <div className={styles.msg__details}>
+          <p>3:20pm</p>
+          <p>3</p>
+        </div>
+      </div>
     </div>
   );
 };
